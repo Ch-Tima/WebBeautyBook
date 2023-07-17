aef5eadd6460defd69e06ee1b310a20d22412cc3
Ch-Tima <92593259+Ch-Tima@users.noreply.github.com>
2023-07-17 10:22:50 +0200

    Added: swagger and azure

--- WebBeautyBook/Program.cs
+++ WebBeautyBook/Program.cs
@@ -1,8 +1,69 @@
+using Azure.Identity;
+using BLL.Infrastructure;
+using BLL.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.FileProviders;
+using WebBeautyBook.Converter;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Azure
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
+
+//Add Services
+builder.Services.AddTransient<ClientService>();
+builder.Services.AddTransient<BaseUserService>();
+builder.Services.AddTransient<CommentService>();
+builder.Services.AddTransient<LocationService>();
+builder.Services.AddTransient<RecordService>();
+builder.Services.AddTransient<ScheduleService>();
+builder.Services.AddTransient<ServiceService>();
+builder.Services.AddTransient<WorkerService>();
+builder.Services.AddTransient<WorkerServiceService>();
+builder.Services.AddTransient<CategoryService>();
+builder.Services.AddTransient<CompanyService>();
+
+
+//Fixes looping navigation fields.
+//[JsonIgnore]
+///A possible object cycle was detected. This can either be due to a cycle or if the object depth is larger than the maximum allowed depth of 32. 
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.Converters.Add(new TimeOnlyJsonConverter());
+    });
+
+//Set SendGridEmailSenderOption and Service
+builder.Services.AddTransient<IEmailSender, SendGridEmailService>();
+
+builder.Services.Configure<Domain.Models.SendGridEmailSenderOption>(opt =>
+{
+    opt.ApiKey = builder.Configuration.GetValue<string>("SGKey");
+    opt.SenderEmail = "logologi417@gmail.com";
+    opt.SenderName = "Tima";
+});
+
+
+builder.Services.Configure(builder.Configuration);
+
+// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//add swagger
+string apiVersion = "v1";
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc(apiVersion,
+        new Microsoft.OpenApi.Models.OpenApiInfo()
+        {
+            Title = "BeautyBookApi",
+            Description = $"API {apiVersion}",
+            Version = apiVersion
+        });
+});
 
 var app = builder.Build();
 
@@ -12,15 +73,28 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//add swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(opt =>
+{
+    opt.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", "BeautyBookApi");
+});
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+            Path.Combine(app.Environment.ContentRootPath, "wwwroot")),
+    RequestPath = "/wwwroot"
+});
+
+app.UseAuthentication();
 app.UseRouting();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
+app.UseAuthorization();
+app.UseHttpsRedirection();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.MapFallbackToFile("index.html"); ;
 
