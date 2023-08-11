db2f5cc484326948f592c6d9cdb8e7f2baf2fbbf
Ch-Tima <92593259+Ch-Tima@users.noreply.github.com>
2023-08-11 17:08:37 +0200

    Account&settings

---
+++ WebBeautyBook/Models/UserResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebBeautyBook.Models
+{
+    public class UserResponseModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Photo { get; set; }
+        public IList<string> Roles { get; set; }
+        public string? WorkerId { get; set; }
+        public string? CompanyId { get; set; }
+    }
+}
---
+++ WebBeautyBook/Validations/MaxFileSizeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBeautyBook.Validations
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSizeAttribute(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        public override bool IsValid(object? value)
+        {
+            var file = value as IFormFile;
+            if (file == null) return true;
+
+            if (file.Length > _maxFileSize) return false;
+
+            return true;
+        }
+    }
+}
