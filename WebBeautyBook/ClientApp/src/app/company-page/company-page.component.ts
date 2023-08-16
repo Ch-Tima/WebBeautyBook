294fd54e32c513662a67207d30b54b4f6dc07a43
Ch-Tima <92593259+Ch-Tima@users.noreply.github.com>
2023-08-16 12:43:14 +0200

    Added heart in 'company-page'

--- WebBeautyBook/ClientApp/src/app/company-page/company-page.component.ts
+++ WebBeautyBook/ClientApp/src/app/company-page/company-page.component.ts
@@ -4,6 +4,8 @@ import { AuthService } from '../services/auth.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Company } from '../models/Company';
 import { Worker } from '../models/Worker';
+import { CompanyLike } from '../models/CompanyLike';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-company-page',
@@ -18,7 +20,7 @@ export class CompanyPageComponent {
 
   private companyId:string|null;
 
-  constructor(private http: HttpClient, private auth: AuthService, private activeRoute:ActivatedRoute, private rout: Router){
+  constructor(private http: HttpClient, public auth: AuthService, private activeRoute:ActivatedRoute, private rout: Router){
     this.companyId = this.activeRoute.snapshot.queryParams['id'];
 
     if(this.companyId == null || this.companyId == undefined || this.companyId == ''){
@@ -38,26 +40,68 @@ export class CompanyPageComponent {
     }
   }
 
-  private loadCompany(){
-    this.http.get<Company>(`api/Company?id=${this.companyId}`).subscribe(
-      result => {
-        this.company = result
-      }, error => {
-        console.log(error)
-        this.rout.navigate(["/"])
-      }
-    )
+  public onClickLike(event: any){
+    var status = $(event.srcElement).attr("onPressed")
+    if(status == "false"){
+      this.http.post(`api/CompanyLike?companyId=${this.company.id}`, "", {
+        headers: this.auth.getHeadersWithToken()
+      }).subscribe(r => {
+        this.company.isFavorite = true;
+      }, e => {
+        alert(e.error);
+      })
+    }else{
+      this.http.delete(`api/CompanyLike?companyId=${this.company.id}`, {
+        headers: this.auth.getHeadersWithToken()
+      }).subscribe(r => {
+        this.company.isFavorite = false;
+      }, e => {
+        alert(e.error);
+      })
+    }
   }
 
-  private loadWorkers(){
-    this.http.get<Worker[]>(`api/Worker/getWorkersByCompanyId/${this.companyId}`).subscribe(
+  private async getAllMienLikes(){
+    await this.http.get<CompanyLike[]>("api/CompanyLike", {
+      headers: this.auth.getHeadersWithToken()
+    }).toPromise().then(result => {
+      var r = result?.findIndex(x => x.companyId == this.company.id)
+      if(r != undefined && r != -1){
+        this.company.isFavorite = true;
+        console.log("ok");
+      }
+    }).catch(e => {
+      console.log(e);
+    });
+  }
+
+  private async loadCompany(){
+    console.log("s -> loadCompany");
+    await this.http.get<Company>(`api/Company?id=${this.companyId}`).subscribe(
+      async (result) => {
+        this.company = result;
+        console.log("r -> loadCompany");
+        await this.getAllMienLikes();
+      }, error => {
+        console.log(error);
+        this.rout.navigate(["/"]);
+      }
+    )
+    console.log("e -> loadCompany");
+  }
+
+  private async loadWorkers(){
+    console.log("s -> loadWorkers");
+    await this.http.get<Worker[]>(`api/Worker/getWorkersByCompanyId/${this.companyId}`).subscribe(
       result => {
         this.workers = result;
+        console.log("r -> loadWorkers");
       }, error => {
         console.log(error);
         console.log("error -> loadWorkers");
       }
     )
+    console.log("e -> loadWorkers");
   }
 
 }
